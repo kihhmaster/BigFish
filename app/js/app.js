03fe11ea9fe71709ea6ca5a8054f6e06810c371b
kihhmaster <kihhmaster@yandex.ru>
2022-09-29 23:18:59 +0300

    Закончил анимацию ежика и волн

---
+++ app/js/app.js
@@ -0,0 +1,96 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+	const followCursor = () => { // объявляем функцию followCursor
+    const el = document.querySelector('.section__seaUrchin') // ищем элемент, который будет следовать за курсором
+		if (el != null) {
+			el.addEventListener('mousemove', e => { // при движении курсора
+				const target = e.target // определяем, где находится курсор
+				if (!target) return;
+				let	seaUrchinStart = document.querySelector('.seaUrchin__start')
+				seaUrchinStart.style.opacity = "0"
+				let	seaUrchinfinish = document.querySelector('.seaUrchin__finish')
+				seaUrchinfinish.style.opacity = "1"
+				let seaUrchin = document.querySelector('.seaUrchin__finish')
+				if (target) { // если курсор наведён на ссылку
+					seaUrchin.classList.add('seaUrchin__active') // элементу добавляем активный класс
+				} else{ // иначе
+					seaUrchin.classList.remove('seaUrchin__active')
+					console.log("иначе")
+				}
+				seaUrchin.style.left = e.pageX + 'px' // задаём элементу позиционирование слева
+				seaUrchin.style.top = e.pageY + 'px'
+			})
+			el.addEventListener("mouseout", e => {
+				const target = e.target
+				if (!target) return;
+				let seaUrchin = document.querySelector('.seaUrchin')
+				seaUrchin.classList.remove('seaUrchin__active')
+			})
+		}
+    
+  }
+
+  followCursor() // вызываем функцию followCursor
+
+	let seaUrchinFinish = document.querySelector(".seaUrchin__finish")
+	seaUrchinFinish.addEventListener('click', () => {
+		console.log('seaUrchin__finish')
+		let weveGreen = document.querySelectorAll(".weve__green")
+		let waveAzure = document.querySelectorAll(".wave__azure")
+		let waveBlue = document.querySelectorAll(".wave__blue")
+		waveBlue.forEach((el)=> {
+			el.classList.add('weve__Blue__y');
+			function say() {
+				el.classList.remove('weve__Blue__y');
+			}
+			setTimeout(()=> {
+				say()
+			},2800);
+		})
+		waveAzure.forEach((el)=> {
+			el.classList.add('weve__Azure__y');
+			function say() {
+				el.classList.remove('weve__Azure__y');
+			}
+			setTimeout(()=> {
+				say()
+			},3300);
+		})
+		weveGreen.forEach((el)=> {
+			el.classList.add('weve__Green__y');
+			function say() {
+				el.classList.remove('weve__Green__y');
+			}
+			setTimeout(()=> {
+				say()
+			},3800);
+		})
+
+	});
+
+	document.querySelector(
+	"body").addEventListener("mousemove", eyeball);
+	function eyeball() {
+		var eye = document.querySelectorAll(".eye");
+		if (eye !=null) {
+			eye.forEach(function (eye) {
+				let x =
+				eye.getBoundingClientRect().left + eye.clientWidth / 2;
+				let y =
+				eye.getBoundingClientRect().top + eye.clientHeight / 2;
+				let radian =
+				Math.atan2(event.pageX - x, event.pageY - y);
+				let rot =
+				radian * (180 / Math.PI) * -1 + 270;
+				eye.style.transform =
+				"rotate(" + rot + "deg)";
+				});
+		}
+
+	}
+
+
+
+})
